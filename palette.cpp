400ba38f0fa12e2e5e2aef54509ec7ebe883ee37
huziqin <ziqinhu@21cn.com>
2020-11-26 22:15:15 +0800

    add new file.

---
+++ palette.cpp
@@ -0,0 +1,19 @@
+const UINT32 palette_16color[] =
+{
+    __palette4F(   0,  255,  255, 0), // 0x0 translater //chenshuixiu 120720 for mantis 187211
+    __palette4F(  71,  71,    71, 0xff), // 0x1 gray
+    __palette4F(  81,  81,    81, 0xff), // 0x2 gray
+    __palette4F(  122,  167,    134, 0xff), // 0x3 no used
+    __palette4F(  92,  184,    0, 0xff), // 0x4 blue-green
+    __palette4F(  0,  255,    0, 0xff), // 0x5 blue
+    __palette4F( 255,  0,  255, 0xff), // 0x6 AV in Yellow
+    __palette4F( 0,  0,  255, 0xff), // 0x7 AV in Red
+    __palette4F( 255,  255,  255, 0xff), // 0x8 white
+    __palette4F( 136,  0,  0, 0xff), // 0x9 Light-Gray
+    __palette4F( 51,  51,  51, 0xff),  // 0xa normal item color
+    __palette4F(  91,   91,   91, 0xff),  // 0xb normal item color
+    __palette4F(  92,  184,    0, 0xff),
+    __palette4F(  30,   30,   30, 0xde),  // 0xd item's circle normal outline
+    __palette4F(  21,   21,   21, 0xde),  // 0xe source bg
+    __palette4F(   0,    0,    0, 0xf0),  // 0xf item's circle black    
+};
